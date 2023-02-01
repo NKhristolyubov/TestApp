599cde7cc7d505421677eadbfbaf11210095e01f
Nikolai Khristoliubov <n.khristoliubov@gmail.com>
2023-02-01 11:01:05 +0400

    added navigation bar

---
+++ TestApp/Applications/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TestApp
+//
+//  Created by Nikolai Khristoliubov on 01.02.23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let navigationController = UINavigationController(rootViewController: MainViewController())
+        if #available(iOS 13.0, *) {
+            navigationController.navigationBar.scrollEdgeAppearance = navigationController.navigationBar.standardAppearance
+        }
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
